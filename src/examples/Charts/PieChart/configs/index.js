3bfcc21495fe1313ebc0b6657f35055c8bbfcfbb
1911481-Wang Yunhao <1595014015@qq.com>
2023-01-31 16:09:56 +0800

    添加扇形图legend

--- src/examples/Charts/PieChart/configs/index.js
+++ src/examples/Charts/PieChart/configs/index.js
@@ -63,7 +63,7 @@ function configs(labels, datasets) {
       maintainAspectRatio: false,
       plugins: {
         legend: {
-          display: false,
+          display: true,
         },
       },
       interaction: {
